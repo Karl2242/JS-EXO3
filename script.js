7293a467caf1c6e506c7516b86215f3c070275fc
Karl2242 <kchikhicloud@gmail.com>
2024-11-15 11:12:10 +0100

    exo 2

---
+++ script.js
@@ -0,0 +1,22 @@
+// let monNombre = parseInt(prompt("Donne moi ton chiffre"))
+
+// for(let i = 1; i <= monNombre; i++){ // compte autant que le chiffre donnée
+//  let etoile = "";
+
+// for(let x = 0; x < i; x++){
+//      etoile += "*";
+// }   
+// console.log(etoile);
+// }
+
+
+// let superHero = ["Spiderm-man", "Captain-America", "Wolverine"
+//     , "Jeremy-Prime-Dev", "Toobo-Le-Bonobo"
+// ]
+
+
+// superHero.forEach((Element) => {
+
+// console.log("Je suis fan de " + Element)
+
+// })
